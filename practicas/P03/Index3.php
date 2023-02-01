658e1629ce2c82f7d91aa31fbc1374dddf59c97b
Art134 <aser_sd@gmail.com>
2023-02-01 14:20:59 -0600

    index3

---
+++ practicas/P03/Index3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <?php
+    $z[] = &$a;
+    echo $a = 'PHP5 <br>';
+    echo $b = '5ta version de php';
+    $c = $b*10;
+    $a .= $b;
+    $b *= $c;
+    echo $z[0] = 'MySQL';
+    ?>
+    </body> 
+    </html>
