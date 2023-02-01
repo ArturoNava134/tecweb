df5ca05d57727f3358d6ec8e4ac995f5b6107ed2
Art134 <aser_sd@gmail.com>
2023-02-01 14:21:47 -0600

    index7

---
+++ practicas/P03/Index7.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <?php
+    echo "Apache version: " . $_SERVER['SERVER_SOFTWARE'] . PHP_EOL;
+    echo "<br>PHP version: " . PHP_VERSION.PHP_EOL;
+    echo "<br>OS: " . PHP_OS . PHP_EOL;
+    ?>
+    </body>
+    </html>
+   
