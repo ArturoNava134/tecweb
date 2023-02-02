2add4c7d75cd2e491884f7c35e994d6bc3a73a11
Art134 <aser_sd@gmail.com>
2023-02-01 19:37:44 -0600

    index2fixed

--- practicas/P03/index2.php
+++ practicas/P03/index2.php
@@ -3,12 +3,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
 <html>
 <head>
-    <title>Práctica 2</title>
+    <title>Práctica 3</title>
 </head>
 <body>
     <h2>Inciso 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
     <?php 
     $a = "manejadorql";
     echo "$a<br>";
@@ -18,6 +17,16 @@
 
     $c = "&$a";
     echo "$c<br>";
+
+    $a = 'PHP server';
+    $b = &$a;
+    $espacio = '<br>';
+    echo $espacio;
+    echo 'Segundo bloque';
+    echo $espacio;
+    echo $a;
+    echo $espacio;
+    echo $b;
     ?>
     </body>
     </html>
