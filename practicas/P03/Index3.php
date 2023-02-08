39375e00219fc54e65728c92e0541d789c69362e
Art134 <aser_sd@gmail.com>
2023-02-07 21:21:08 -0600

    Index3_p03

--- practicas/P03/Index3.php
+++ practicas/P03/Index3.php
@@ -1,20 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
 <html>
 <head>
-    <title>Práctica 2</title>
+    <title>Práctica 3</title>
 </head>
 <body>
-    <h2>Inciso 2</h2>
-    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
-    <?php
-    $z[] = &$a;
-    echo $a = 'PHP5 <br>';
-    echo $b = '5ta version de php';
-    $c = $b*10;
-    $a .= $b;
-    $b *= $c;
-    echo $z[0] = 'MySQL';
+    <h2>Inciso 3</h2>
+    <p>Muestra el contenido de cada variable inmediatamente después de cada asignación,
+verificar la evolución del tipo de estas variables (imprime todos los componentes de los
+arreglo):</p>
+<?php
+       $a = "PHP5";
+       echo "$a<br>";
+       $z[] = "&$a";
+       echo "$z<br>";
+       $b = "5a version de PHP";
+       echo "$b<br>";
+       $c = $b*10;
+       echo "$c<br>";
+       $a .= $b;
+       echo "$a .<br>";
+       $b *= $c;
+       echo "$b *[0]<br>";
+       $z[0] = "MySQL";
+       echo "$z[0]<br>";
     ?>
     </body> 
     </html>
