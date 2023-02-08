74ed99398df917616918aebb90219f5dbcf68b06
Art134 <aser_sd@gmail.com>
2023-02-07 21:22:33 -0600

    Index6_p03

--- practicas/P03/Index6.php
+++ practicas/P03/Index6.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
 <html>
 <head>
-    <title>Práctica 2</title>
+    <title>Práctica 3</title>
 </head>
 <body>
-    <h2>Inciso 2</h2>
-    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <h2>Inciso 6</h2>
+    <p>Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y muéstralas
+    usando la función var_dump(<datos>).</p>
     <?php
     $a = '0';
     $b = 'TRUE';
